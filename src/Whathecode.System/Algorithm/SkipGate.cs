258249248bae4c988138c580f3b6eeb8041b7f73
Steven Jeuris <steven.jeuris@gmail.com>
2016-09-23 01:33:02 +0200

    Migrated System.Algorithm and dependent IntervalCollection.

---
+++ src/Whathecode.System/Algorithm/SkipGate.cs
@@ -0,0 +1,29 @@
+﻿namespace Whathecode.System.Algorithm
+{
+	/// <summary>
+	/// A gate which skips a specified amount of entries before opening.
+	/// </summary>
+	public class SkipGate : AbstractGate
+	{
+		readonly int _skipCount;
+		int _curCount;
+
+
+		public SkipGate( int skipCount, bool autoReset = false )
+			: base( autoReset )
+		{
+			_skipCount = skipCount;
+		}
+
+
+		protected override bool TryEnterGate()
+		{
+			return ++_curCount > _skipCount;
+		}
+
+		protected override void ResetGate()
+		{
+			_curCount = 0;
+		}
+	}
+}
