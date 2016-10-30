d711dd6f6d6d14b08ad3249a0a79227d11540398
Steven Jeuris <steven.jeuris@gmail.com>
2016-10-30 16:09:12 +0100

    Added 'GetIndexInterval' for array.

---
+++ src/Whathecode.System/Extensions.Array.cs
@@ -0,0 +1,16 @@
+﻿namespace Whathecode.System
+{
+	public static partial class Extensions
+	{
+		/// <summary>
+		/// Returns the interval of the indices.
+		/// </summary>
+		/// <typeparam name = "T">Type of the values in the array.</typeparam>
+		/// <param name = "source">The source for this extension method.</param>
+		/// <returns>The interval of the indices of the array.</returns>
+		public static IInterval<int> GetIndexInterval<T>( this T[] source )
+		{
+			return new IntInterval( 0, source.Length - 1 );
+		}
+	}
+}
